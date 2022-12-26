1504460e1394e68fb4c18a6d84255276ea90d1ec
skilalis <skilalis@gmail.com>
2022-12-27 01:51:47 +0600

    Initial commit

---
+++ app/src/main/java/com/example/mysharedemo/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.mysharedemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.manu_item,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.shareId) {
+            
+        }else if (item.getItemId() == R.id.feedbackId) {
+            Intent intent = new Intent(getApplicationContext(),FeedbackActivity.class);
+            startActivity(intent);
+        }
+
+        
+        return super.onOptionsItemSelected(item);
+    }
+}
